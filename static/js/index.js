ea9cbb1ee7fe7e99a567974a3878a5f995fbb573
rmaratos <rmaratos@andrew.cmu.edu>
2016-07-04 17:07:27 -0700

    Cleanup code

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+//backround settings
+$('body').vegas({
+delay: 6000,
+transition: 'fade',
+timer: false,
+transitionDuration: 3000,
+preload: true,
+slides: [
+    { src: "static/img/gallery/griz-sc2016.jpg" },
+    { src: "static/img/gallery/lg2016.jpg" },
+    { src: "static/img/gallery/gs_sound.jpg" },
+    { src: "static/img/gallery/dejj-2014.jpg" },
+    { src: "static/img/gallery/car_12.jpg" },
+    { src: "static/img/gallery/outdoorConcert.jpg" },
+    { src: "static/img/gallery/ds.jpg" },
+    { src: "static/img/gallery/ds_12.jpg" },
+    { src: "static/img/gallery/lunarGala.png" },
+    { src: "static/img/gallery/timeflies-2014.jpg" },
+]
+});
