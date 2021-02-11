72f644c48f464f6fe585a2d01c932e3e967d9a1c
Perry Naseck <DaAwesomeP@users.noreply.github.com>
2021-02-11 18:30:44 -0500

    static backup

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+//backround settings
+$('body').vegas({
+delay: 10000,
+transition: 'fade',
+timer: false,
+transitionDuration: 3000,
+preload: true,
+slides: [
+    { src: "static/img/gallery/griz-sc2016.jpg" },
+    { src: "static/img/gallery/lg2016.jpg" },
+    { src: "static/img/gallery/gs_sound.jpg" },
+    { src: "static/img/gallery/dejj-2014.jpg" },
+    { src: "static/img/gallery/ds.jpg" },
+]
+});
+
